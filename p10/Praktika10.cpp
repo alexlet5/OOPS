538b25164b40086afab044f2a44455fa1fe215c9
Alex <alex.letkeman@mail.ru>
2022-10-02 17:48:11 +0300

    ?

--- p10/Praktika10.cpp
+++ p10/Praktika10.cpp
@@ -9,3 +9,41 @@
 #include "../utils/ArrayUtils.h"
 
 using namespace std;
+
+struct node {
+    unsigned char info;
+    struct node *next;
+};
+
+//Используя  динамическую  структуру  список,
+// подсчитать количество  элементов  лежащих  в  диапазоне
+// от  20  до  50. Записать их в отдельный динамический массив.
+// Полученный массив вывести на экран.
+void Praktika10::run() {
+
+    srand(time(nullptr));// без этого числа будут одинаковые
+    typedef node *NodePtr;
+    NodePtr head = nullptr;
+    NodePtr tek;
+
+    NodePtr hvost; // указатель на "хвост" списка
+    int N = 50;
+    int cnt = 1;
+    if (head == nullptr) {
+        head = new node;
+        head->info = char(rand() % 255); // случайныйсимвол
+        head->next = nullptr;
+        hvost = head;
+    }
+
+    for (int i = 2; i <= N; i++) {
+        tek = new node;
+        ++cnt;
+        tek->info = char(rand() % 255);
+        tek->next = nullptr;
+        hvost->next = tek;
+        // вданномслучае-NULL
+        hvost = tek;
+    }
+
+}
